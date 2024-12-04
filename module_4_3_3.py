25c52f9467fb755c1d1610b8c2a99579dcc19f4f
MaxDev2024 <maksimkalacky2009@gmail.com>
2024-12-04 11:26:47 +0900

    Update module_4_3_3.py

--- module_4_3_3.py
+++ module_4_3_3.py
@@ -13,7 +13,6 @@ class House:
     def __del__(self):
         print(f'{self.name}, снесён, но он останется в истории')
 
-
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
 h2 = House('ЖК Акация', 20)
